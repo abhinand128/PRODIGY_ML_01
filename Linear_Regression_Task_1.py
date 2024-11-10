1570743888aba5b99d7234696a56a2309339340c
abhinand128 <abhinanda128@gmail.com>
2024-11-10 15:13:29 +0530

    final

--- Linear_Regression_Task_1.py
+++ Linear_Regression_Task_1.py
@@ -3,3 +3,40 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Data_Set/train.csv')
+
+X = df[['GrLivArea', 'BedroomAbvGr', 'FullBath']]
+y = df['SalePrice']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = LinearRegression()
+
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Calculate the Mean Squared Error
+mse = mean_squared_error(y_test, y_pred)
+print(f"Mean Squared Error: {mse}")
+
+print("Coefficients:", model.coef_)
+print("Intercept:", model.intercept_)
+
+plt.scatter(y_test, y_pred)
+plt.xlabel("Actual Prices")
+plt.ylabel("Predicted Prices")
+plt.title("Actual vs. Predicted Prices")
+plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red')  # Line of perfect prediction
+plt.show()
+
+square_footage = float(input("Enter the square footage: ")) 
+num_bedrooms = int(input("Enter the number of bedrooms: ")) 
+num_bathrooms = int(input("Enter the number of bathrooms: "))
+
+input_features = [[square_footage, num_bedrooms, num_bathrooms]]
+predicted_price = model.predict(input_features)
+print(f'The predicted price of the house is: {predicted_price[0]}')
